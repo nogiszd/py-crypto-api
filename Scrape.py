cd32dd75ea6f3ca89f6ed0d7ca557d0bfc114c6e
nogiszd <55346068+nogiszd@users.noreply.github.com>
2022-04-20 17:43:01 +0200

    api fix for new google method

--- Scrape.py
+++ Scrape.py
@@ -1,18 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 class Scrape():
     def __init__(self, method, symbol, elements):
         headers = {
-            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582",
         }
+        
+        cookies_dict = {"CONSENT": "YES+"}
         
         match method:
             case 1:
-                res = requests.get("https://www.google.com/search?q=" + symbol.replace(" ", "+"), headers=headers).text
+                res = requests.get("https://www.google.com/search?q=" + symbol.replace(" ", "+"), headers=headers, cookies=cookies_dict).text
+                print(res)
                
             case 2:
-                res = requests.get("https://www.google.com/search?q=" + symbol[0] + "+to+" + symbol[1], headers=headers).text
+                res = requests.get("https://www.google.com/search?q=" + symbol[0] + "+to+" + symbol[1], headers=headers, cookies=cookies_dict).text
 
         soup = BeautifulSoup(res, "html.parser")
         self.__summary = {}
@@ -20,7 +24,8 @@ class Scrape():
         for el in elements["elements"]:
             tag = soup.select_one(el["from"])
             if tag != None:
-                self.__summary[el["to"]] = "{:.2f}".format(float(tag["value"]))        
+                value = re.sub("[^0-9\,]", "", tag.getText())
+                self.__summary[el["to"]] = "{:.2f}".format(float(value.replace(",", ".")))
 
     def summary(self):
         return self.__summary
