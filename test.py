e23e4040eef83dd8da1de6e372d163956b0ee1b5
nogiszd <55346068+nogiszd@users.noreply.github.com>
2022-06-24 18:16:17 +0200

    fix test

--- test.py
+++ test.py
@@ -7,12 +7,3 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"hello":"world"}
-    
-def test_read_main_fetch():
-    response = client.get("/v1/1+btc+to+usd")
-    assert response.status_code == 200
-    
-def test_read_main_empty_query():
-    response = client.get("/v1/empty")
-    assert response.status_code == 400
-    assert response.json() == {"detail": "empty query is empty"}
